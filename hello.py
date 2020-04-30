04883c003ef082cd0e8a60a479868c689f23678b
eliseipanov <53929996+eliseipanov@users.noreply.github.com>
2020-04-30 23:23:27 +0200

    request and response learning

--- hello.py
+++ hello.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask import request
+from flask import make_response
 
 app = Flask(__name__)
 @app.route('/')
 def index():
+    response = make_response('<h2>This document carries a coockie</h2>')
+    response.set_cookie('answer','42')
+    return response
+"""
     user_agent = request.headers.get('User-agent')
     return '<p>Your browser agent is {}</p>'.format(user_agent)
+
 
 @app.route('/user/<name>')
 def user(name):
     return '<H1>Hello, {}!</H1>'.format(name)
-
+"""
 
