6c45eff9dc0aa9f5a98b7a9d0ab67b6ef47dfd69
eliseipanov <53929996+eliseipanov@users.noreply.github.com>
2020-04-30 22:52:57 +0200

    User agent how to

--- hello.py
+++ hello.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return '<H1>Hello, Alex!</H1>'
+    user_agent = request.headers.get('User-agent')
+    return '<p>Your browser agent is {}</p>'.format(user_agent)
+
+@app.route('/user/<name>')
+def user(name):
+    return '<H1>Hello, {}!</H1>'.format(name)
+
+
