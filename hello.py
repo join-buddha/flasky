16a343039690cd2dd422aa411667f08854db6aa9
eliseipanov <53929996+eliseipanov@users.noreply.github.com>
2020-05-03 14:48:54 +0200

    user html template modigied to use flask bootstrap4

--- hello.py
+++ hello.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-#from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+boostrap = Bootstrap(app)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name.capitalize())
