73b9448a5dd6a1614e562ba70793a27e59103a21
eliseipanov <53929996+eliseipanov@users.noreply.github.com>
2020-05-04 13:52:10 +0200

    commit application.py and cmd changes

--- application.py
+++ application.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
 application = app = Flask(__name__)
-
 boostrap = Bootstrap(app)
 
 @app.route('/')
@@ -11,3 +10,6 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name.capitalize())
+"""
+some changes made
+"""
