c44294804ccf47b10fca31d1c3ff643893e084f8
eliseipanov <53929996+eliseipanov@users.noreply.github.com>
2020-05-04 11:56:33 +0200

    commit hello.py to application.py for Beanstalk env

--- application.py
+++ application.py
@@ -8,7 +8,6 @@ boostrap = Bootstrap(app)
 @app.route('/')
 def index():
     return render_template('index.html')
-
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name.capitalize())
